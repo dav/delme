b85cfde84ee0e71f003d9cb6d59f38c1d9f8b230
Dav Yaginuma <dav@akuaku.org>
2015-08-02 13:17:24 -0700

    Updated script

--- create_pr.sh
+++ create_pr.sh
@@ -2,14 +2,35 @@ DATE=`date +"%Y-%m-%d--%M-%S-%s"`
 BRANCH="branch-${DATE}"
 FILENAME="change.me"
 
+echo
+echo "*** creating new branch..."
+echo
 git checkout -b "${BRANCH}"
 
 echo "Change ${DATE}" > "${FILENAME}"
 
+echo
+echo "*** committing change on branch..."
+echo
 git add "${FILENAME}"
 git commit -m "Updated ${FILENAME}\n[Refs #100374104]"
+
+echo
+echo "*** Push branch to origin..."
+echo
 git push origin "${BRANCH}"
 
+echo
+echo "*** Creating pull request..."
+echo
+hub pull-request -m "Made change via script"
+
+echo
+echo "*** Switching back to master..."
+echo
 git checkout master
 
-git branch -d "${BRANCH}"
+echo
+echo "*** Deleting local branch..."
+echo
+git branch -D "${BRANCH}"
